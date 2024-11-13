7dd6b54e4c34510cba4e54cd03548afaf3216fcc
Rebecca-de-Boer <rebecca.de-boer@mths.ca>
2024-11-13 11:17:13 -0500

    makes a square

--- main.ts
+++ main.ts
@@ -1,8 +1,31 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
+ * Created by: Rebecca de Boer
+ * Created on: Oct 2024
  * This program ...
 */
+// variables
+let sprite: game.LedSprite = null
+let loopCounter = 0
 
-basic.showString('Hello, World!')
+// setup
+basic.showIcon(IconNames.Happy)
+basic.pause(500)
+basic.clearScreen()
+
+basic.clearScreen()
+  loopCounter = 0
+  sprite = game.createSprite(0, 0)
+
+  while (loopCounter <= 5) {
+    sprite.set(LedSpriteProperty.X, loopCounter)
+    sprite.set(LedSpriteProperty.Y, loopCounter)
+    loopCounter = loopCounter = 1
+    basic.pause(500)
+    loopCounter = 0
+    sprite = game.createSprite (5, 0)
+      while (loopCounter <= 0)
+  sprite.delete()
+  basic.showIcon(IconNames.Happy)
+
+}
